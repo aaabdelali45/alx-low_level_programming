c6f790081373601e245d60e27a9795fa434f6225
aaabdelali980 <aliabdel726@gmail.com>
2023-03-30 19:25:55 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strncat -  function is similar to the _strcat function
+ * except that it will use at most n bytes from src
+ * and src does not need to be null-terminated if
+ * it contains n or more bytes
+ * @dest: input
+ * @src: input
+ * @n: input
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
